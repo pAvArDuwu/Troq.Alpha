fd7e83e0aab1b5a46be5e2c538f9628ceff21903
benjamin <benjaminvillegasrojas@gmail.com>
2025-04-23 17:14:01 -0400

    Proyecto Troc

--- app/src/main/java/com/example/miapicacion/MainActivity.java
+++ app/src/main/java/com/example/miapicacion/MainActivity.java
@@ -1,19 +1,13 @@
 package com.example.miapicacion;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable();
-
+        setContentView(R.layout.activity_main); // Asegurate de que tengas este layout
     }
 }
